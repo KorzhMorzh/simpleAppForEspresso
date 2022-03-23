2825ad574bb8bf0b2882940b5cbd74d936ca4187
Anna Malinina <anna-maks99@lenta.ru>
2022-03-23 18:38:04 +0100

    lab 3

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
@@ -16,8 +16,10 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.hamcrest.Matchers.allOf;
 
+import android.Manifest;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
 import android.view.Gravity;
 
 import androidx.test.espresso.IdlingRegistry;
@@ -26,19 +28,46 @@ import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.uiautomator.UiDevice;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
 import org.junit.runner.RunWith;
 
-@RunWith(AndroidJUnit4.class)
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+import io.qameta.allure.kotlin.Allure;
+
+@RunWith(AllureAndroidJUnit4.class)
 public class MainActivityTest {
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityTestRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Rule
+    public TestWatcher watcher = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, org.junit.runner.Description description) {
+            String className = description.getClassName();
+            className = className.substring(className.lastIndexOf('.') + 1);
+            String methodName = description.getMethodName();
+            takeScreenshot(className + "#" + methodName);
+        }
+    };
+
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule
+            .grant(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            );
 
     @Before
     public void registerIdlingResources() {
@@ -52,6 +81,7 @@ public class MainActivityTest {
 
     @Test
     public void testGallery() {
+        Allure.feature("openGallery");
         ViewInteraction drawer = onView(withId(R.id.drawer_layout));
         drawer.check(matches(isClosed(Gravity.START)))
                 .perform(DrawerActions.open());
@@ -64,6 +94,7 @@ public class MainActivityTest {
 
     @Test
     public void testFABVisibility(){
+        Allure.feature("FabVisibility");
         ViewInteraction drawer = onView(withId(R.id.drawer_layout));
         ViewInteraction nav = onView(withId(R.id.nav_view));
         ViewInteraction fab = onView(withId(R.id.fab));
@@ -82,6 +113,7 @@ public class MainActivityTest {
 
     @Test
     public void testGalleryList() {
+        Allure.feature("galleryList");
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.nav_gallery)).perform(click());
         onView(withId(R.id.recycle_view)).check(ViewAssertions.matches(SizeMatcher.of(10)));
@@ -89,6 +121,7 @@ public class MainActivityTest {
 
     @Test
     public void testSettings() {
+        Allure.feature("SettingsIntent");
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
         ViewInteraction settings = onView(withText(R.string.action_settings));
         Intents.init();
@@ -100,6 +133,21 @@ public class MainActivityTest {
                 hasData(Uri.parse("https://google.com"))
         ));
         Intents.release();
+    }
+
+    private void takeScreenshot(String name) {
+        File path = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/screenshots/");
+        if (!path.exists()) {
+            path.mkdirs();
+        }
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        String filename = name + ".png";
+        device.takeScreenshot(new File(path, filename));
+        try {
+            Allure.attachment(filename, new FileInputStream(new File(path, filename)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 }
